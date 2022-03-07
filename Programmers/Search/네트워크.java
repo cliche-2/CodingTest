82df59aa92254fccd8abcce37d34f68e43bedaf3
cliche-2 <kaeunl33@gmail.com>
2022-03-08 00:59:17 +0900

    Pass

---
+++ Programmers/Search/네트워크.java
@@ -0,0 +1,41 @@
+/***
+ * 
+ * https://programmers.co.kr/learn/courses/30/lessons/43162#
+ * DFS - 네트워크
+ * 
+ */
+
+import java.util.Arrays;
+
+class Solution {
+	boolean[] isVisited;
+
+	public int solution(int n, int[][] computers) {
+
+		int answer = 0;
+		isVisited = new boolean[n];
+		Arrays.fill(isVisited, false);
+
+		for (int i = 0; i < computers.length; i++) {
+			if (isVisited[i])
+				continue;
+			answer += dfs(i, computers);
+		}
+		return answer;
+	}
+
+	int dfs(int start, int[][] computers) {
+
+		isVisited[start] = true;
+
+		for (int i = 0; i < computers.length; i++) {
+			if (start == i || isVisited[i])
+				continue;
+			if (computers[start][i] == 1) {
+				dfs(i, computers);
+			}
+		}
+		return 1;
+	}
+
+}
